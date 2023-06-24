3d288574917816e319d3f3aacc8e10da11ee9a95
Sterben <86081863+Sterben22@users.noreply.github.com>
2023-06-23 20:51:27 -0500

    Limpieza

--- gameManager/enemy.py
+++ gameManager/enemy.py
@@ -1,32 +1,37 @@
+import random
 
 import pygame
 from pygame.locals import *
 
+from .collision import *
+from .config import *
+from .menu import *
+
 class Enemy():
-    def __init__(self, x, y, path) -> None:
-        self.pos = self.x,self.y = x,y
-        self.change = self.change_x,self.change_y = 1.2 , 1.5
-        self._sprite = pygame.image.load(path).convert()
-        self.w = self._sprite.get_width()
-        self.h = self._sprite.get_height()
-        self.life = True
+    def __init__(self, config: EnemyConfig):
+        self.pos = random.randint(64, 737), random.randint(30, 180)
+        self.change = 2, 2
+        self._sprite = pygame.image.load(config.SKIN).convert()
     
-
     def move(self, player):
-        if self.y >= 450:
-            if abs(player.x - self.x) < 80:
-                self.y = 2000
-                
-                pass
+        x = self.pos[0]
+        y = self.pos[1]
+
+        if self.pos[1] >= 600:
+            x = random.randint(64, 737)
+            y = 0
         
-        if self.x <= 735 or self.x>=0:
-            self.x += self.change_x
-            self.change_x *= -1
-            self.y += self.change_y
+        if self.pos[0] <= 735 or self.pos[1] >= 0:
+            xc = self.change[0]
+            xc *= -1
+            # Update change
+            self.change = xc, self.change[1]
+
+            x += self.change[0]
+            y += self.change[1]
             
-            self.pos = self.x,self.y
+            # Update position
+            self.pos = x, y
 
-
-
-if __name__ == "__main__" :
-    theApp = Enemy()
+    def render(self, surface):
+        surface.blit(self._sprite, self.pos)
