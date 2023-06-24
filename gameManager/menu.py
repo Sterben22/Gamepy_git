462042fe2a07a1efb63802a0452cece02ee11a55
Sterben <86081863+Sterben22@users.noreply.github.com>
2023-06-23 20:53:18 -0500

    Limpieza

--- gameManager/menu.py
+++ gameManager/menu.py
@@ -4,9 +4,11 @@ from pygame.locals import *
 def game_over(screen):
     game_over_text = pygame.font.Font('freesansbold.ttf', 64).render("GAME OVER", True, (255,255,255))
     screen.blit(game_over_text, (190, 250))
-    
+    pygame.display.flip()    
 
 def pause(screen):
     game_over_text = pygame.font.Font('freesansbold.ttf', 64).render("PAUSE", True, (255,255,255))
-    screen.blit(game_over_text, (190, 250))
+    screen.blit(game_over_text, (280, 260))
+    pygame.display.flip()
+    
     
